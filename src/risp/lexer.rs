63883c5a079f7cb19612219ecf1d08c1d6a02ecf
shivrm <shivrm@proton.me>
2022-04-28 16:20:22 +0530

    Add `next_token` method to Lexer

--- src/risp/lexer.rs
+++ src/risp/lexer.rs
@@ -21,4 +21,55 @@ impl<'a> Lexer<'a> {
     fn current_char(&self) -> Option<char> {
         self.chars.clone().next()
     }
+    
+    fn read_number(&mut self) -> i32 {
+        todo!();
+    }
+
+    fn read_name(&mut self) -> String {
+        todo!();
+    }
+
+    pub fn next_token(&mut self) -> Token {
+        if let Some(c) = self.current_char() {
+            
+            // Skip character if it is whitespace
+            if c.is_whitespace() {
+                self.advance();
+                self.next_token()
+
+
+            } else if c.is_numeric() {
+                let value = self.read_number();
+                Token {
+                    kind: TokenKind::Number,
+                    value: Box::new(value)
+                }
+
+
+            } else if c.is_alphabetic() {
+                let value = self.read_name();
+                Token {
+                    kind: TokenKind::Name,
+                    value: Box::new(value)
+                }
+
+            // Miscellaneous single-character tokens
+            } else {
+                self.advance();
+                let kind = match c {
+                    '(' => TokenKind::OpenParen,
+                    ')' => TokenKind::CloseParen,
+                    _ => panic!("Unknown character {c}")
+                };
+                
+                Token {
+                    kind,
+                    value: Box::new(c)
+                }
+            }
+        } else {
+            panic!("EOF")
+        }
+    }
 }
