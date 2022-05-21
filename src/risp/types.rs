fbc733dc5eb6c7b6fac604f107d712c6b0e53263
shivrm <shivrm@proton.me>
2022-05-21 21:24:54 +0530

    Improve typing system

--- src/risp/types.rs
+++ src/risp/types.rs
@@ -1,46 +1,122 @@
+pub type Int = i32;
+pub type Str = String;
+pub type List = Vec<Type>;
+pub type RustFn = fn(List) -> List;
+pub struct Null;
+
 #[derive(Clone)]
 pub enum Type {
-    Number(i32),
-    String(String),
-    List(Vec<Type>),
-    BuiltinFn(extern fn(Vec<Type>) -> Vec<Type>),
+    Int(Int),
+    Str(Str),
+    List(List),
+    RustFn(RustFn),
     Null,
 }
 
-impl Type {
-    pub fn repr(&self) -> String {
+pub trait RispPrint {
+    fn repr(&self) -> String;
+    fn display(&self) -> String;
+}
+
+// Function definitions could be done by a macro 🤔
+impl RispPrint for Type {
+    fn repr(&self) -> String {
         match self {
-            Type::Number(n) => n.to_string(),
-            Type::String(s) => format!("{s:?}"),
-
-            Type::List(elems) =>  {
-                let mut iter = elems.iter();
-                let mut repr = String::from("[");
-
-                match iter.next() {
-                    Some(el) => repr += &el.repr(),
-                    None => (())
-                }
-
-                for el in iter {
-                    repr += ", ";
-                    repr += &el.repr();
-                }
-
-                repr.push(']');
-                repr
-            }
-
-            Type::BuiltinFn(_) => "<Builtin Function>".to_owned(),
-            Type::Null => "".into()
+            Type::Int(n)    => n.repr(),
+            Type::Str(s)    => s.repr(),
+            Type::List(l)   => l.repr(),
+            Type::RustFn(f) => f.repr(),
+            Type::Null      => Null.repr(),
         }
     }
 
-    pub fn display(&self) -> String {
+    fn display(&self) -> String {
         match self {
-            Type::String(s) => s.clone(),
-            Type::Null => "Null".into(),
-            _ => self.repr()
+            Type::Int(n)    => n.display(),
+            Type::Str(s)    => s.display(),
+            Type::List(l)   => l.display(),
+            Type::RustFn(f) => f.display(),
+            Type::Null      => Null.display(),
         }
+    }
+}
+
+
+impl RispPrint for Int {
+    fn display(&self) -> String {
+        self.to_string()
+    }
+
+    fn repr(&self) -> String {
+        self.to_string()
+    }
+}
+
+impl RispPrint for Str {
+    fn display(&self) -> String {
+        self.clone()
+    }
+
+    fn repr(&self) -> String {
+        format!("{self:?}")
+    }
+}
+
+impl RispPrint for List {
+    fn repr(&self) -> String {
+        let mut iter = self.iter();
+
+        let mut result = match iter.next() {
+            Some(el) => {
+                let r = el.repr();
+                format!(", {r}")
+            },
+            None => String::from("[")
+        };
+
+        for el in iter {
+            result += &format!(", {}", el.repr())
+        }
+
+        result.push(']');
+
+        result
+    }
+
+    fn display(&self) -> String {
+        let mut iter = self.iter();
+
+        let mut result = match iter.next() {
+            Some(el) => format!("[{}", el.display()),
+            None => String::from("[")
+        };
+
+        for el in iter {
+            result += &format!(", {}", el.display())
+        }
+
+        result.push(']');
+
+        result
+    }
+}
+
+impl RispPrint for RustFn {
+    fn display(&self) -> String {
+        "<Rust Function>".to_owned()
+    }
+
+    fn repr(&self) -> String {
+        "<Rust Function>".to_owned()
+    }
+}
+
+impl RispPrint for Null {
+    fn display(&self) -> String {
+        "Null".to_owned()
+    }
+
+    fn repr(&self) -> String {
+        String::new()
     }
 }
