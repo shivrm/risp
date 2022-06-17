6efc2605644bb805444fcc17517dfc658c2ece84
shivrm <shivrm@proton.me>
2022-06-17 11:40:12 +0530

    Refactor lexer

--- src/risp/lexer.rs
+++ src/risp/lexer.rs
@@ -2,29 +2,48 @@ use crate::risp::token::Span;
 use crate::risp::{Error, Kind, Token};
 use std::str::Chars;
 
-/// Struct that represents a lexer, used for producing tokens from a string of text
+/// Splits a source string into tokens
 pub struct Lexer<'a> {
-    /// Iterator over the characters of the text
+    /// Iterator over the characters of the source
     chars: Chars<'a>,
-    /// Current position that the lexer references to generate tokens
+    /// Current position that the lexer uses to construct spans
     pos: usize,
 }
 
 impl<'a> Lexer<'a> {
-    /// Creates a new lexer
-    pub fn new(text: &'a str) -> Self {
+    /// Creates a new lexer from a source string
+    pub fn new(source: &'a str) -> Self {
         Self {
-            chars: text.chars(),
+            chars: source.chars(),
             pos: 0,
         }
     }
 
+    /// Returns the current lexer character, or '\0' if the lexer is at the end of the source
+    #[inline]
+    pub fn current_char(&self) -> char {
+        self.chars.clone().next().unwrap_or('\0')
+    }
+
+    /// Returns true if the lexer is at the end of the source
+    #[inline]
     pub fn eof(&self) -> bool {
         self.chars.clone().next().is_none()
     }
 
-    /// Takes characters from the lexer while a predicate is met
+    /// Advances the lexer to the next character
     #[inline]
+    fn adv(&mut self) {
+        self.pos += match self.chars.next() {
+            Some(c) => c.len_utf8(),
+            None => return,
+        }
+    }
+    
+    /// Advances the lexer while a predicate is met.
+    /// 
+    /// The predicate is a function which takes a `char` as an argument and returns a `bool`.
+    /// The return value determines whether the lexer should be advanced.
     fn take_while(&mut self, mut predicate: impl FnMut(char) -> bool) -> Span {
         let start = self.pos;
 
@@ -33,6 +52,7 @@ impl<'a> Lexer<'a> {
             let mut clone = self.chars.clone();
 
             match clone.next() {
+                // Advance the lexer if the predicate is met
                 Some(c) if predicate(c) => {
                     self.chars = clone;
                     self.pos += c.len_utf8();
@@ -43,101 +63,85 @@ impl<'a> Lexer<'a> {
         }
     }
 
-    /// Advances the lexer to the next character
-    #[inline]
-    fn adv(&mut self) {
-        self.pos += match self.chars.next() {
-            Some(c) => c,
-            None => return,
-        }
-        .len_utf8();
-    }
-
-    /// Gets the next token from the text and returns it
+    /// Gets the next token from the lexer. Since the lexer might throw an error,
+    /// this method returns a `Result`
     #[inline]
     pub fn next(&mut self) -> Result<Token, Error> {
         let start = self.pos;
-        let next = self.chars.clone().next();
 
-        let next = match next {
-            None => {
-                return Ok(Token {
-                    kind: Kind::EOF,
-                    span: Span::new(self.pos, self.pos),
-                })
+        /// A macro that makes it easier to create a token
+        macro_rules! tok {
+            ($kind:expr) => {
+                Token {
+                    span: Span::new(start, self.pos),
+                    kind: $kind
+                }
+            };
+            ($kind:expr, $span:expr) => {
+                Token {
+                    span: $span,
+                    kind: $kind
+                }
+            };
+        }
+
+        let token = match self.current_char() {
+            '\0' if self.eof() => {
+                tok!(Kind::EOF)
             }
-            Some(c) => c,
-        };
 
-        match next {
             // Skip character if it is whitespace
             c if c.is_whitespace() => {
                 self.take_while(|c| c.is_whitespace());
-                self.next()
+                self.next()?
             }
 
-            // Todo: Use a proper number parser here
             '0'..='9' => {
-                let span = self.take_while(|c| matches!(c, '0'..='9'));
+                self.take_while(|c| matches!(c, '0'..='9'));
                 
+                // If the number is followed by a `.`, treat it as a float
                 if let Some('.') = self.chars.clone().next() {
                     self.adv();
                     self.take_while(|c| matches!(c, '0'..='9'));
 
-                    Ok(Token {
-                        span: Span::new(start, self.pos),
-                        kind: Kind::Float
-                    })
+                    tok!(Kind::Float)
                 } else {
-                    Ok(Token {
-                        span,
-                        kind: Kind::Number,
-                    })
+                    tok!(Kind::Number)
                 }
 
             }
 
             'a'..='z' | 'A'..='Z' | '_' => {
-                let span =
-                    self.take_while(|c| matches!(c, 'a'..='z' | 'A'..='Z' | '_' | '0'..='9'));
-                Ok(Token {
-                    span,
-                    kind: Kind::Name,
-                })
+                self.take_while(|c| matches!(c, 'a'..='z' | 'A'..='Z' | '_' | '0'..='9'));
+                tok!(Kind::Name)
             }
 
+            // + and - are matched here because they might be the sign of a number.
             '+' | '-' => {
                 self.adv();
                 
+                // When next token is a number, add this +/- to it. The sign will be parsed by the parser.
                 if let Some('0'..='9') = self.chars.clone().next() {
                     let kind = self.next()?.kind;
-                    Ok(Token {
-                        span: Span::new(start, self.pos),
-                        kind
-                    })
+                    tok!(kind)
                 } else {
-                    Ok(Token {
-                        span: Span::new(start, self.pos),
-                        kind: Kind::Operator,
-                    })
+                    tok!(Kind::Operator)
                 }
             }
 
             '"' => {
                 self.adv();
+
                 // TODO: Add support for escape sequences (might need to split this into a seperate fn)
                 let span = self.take_while(|c| !matches!(c, '"'));
                 self.adv();
-                Ok(Token {
-                    span,
-                    kind: Kind::String,
-                })
+                tok!(Kind::String, span)
             }
 
             // Miscellaneous single-character tokens
             c => {
                 self.adv();
-                let span = Span::new(start, self.pos);
+
                 let kind = match c {
                     '(' => Kind::OpenParen,
                     ')' => Kind::CloseParen,
@@ -145,8 +149,10 @@ impl<'a> Lexer<'a> {
                     _ => return Err(Error::LexError(c)),
                 };
 
-                Ok(Token { span, kind })
+                tok!(kind)
             }
-        }
+        };
+
+        Ok(token)
     }
 }
