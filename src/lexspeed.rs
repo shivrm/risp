c732fa459be335b2d9cffdcc36e8903cb556456b
shivrm <shivrm@proton.me>
2022-06-22 20:53:27 +0530

    Refactor main.rs, update version

---
+++ src/lexspeed.rs
@@ -0,0 +1,61 @@
+use std::time::{Duration, Instant};
+use std::fs;
+use crate::risp::{Lexer, TokenKind};
+
+/// Used to benchmark a function
+fn bench(mut func: impl FnMut(), num: u32, samples: u32) -> Duration {
+    let mut max = Duration::from_secs(0);
+    let mut min = Duration::from_secs(u64::MAX);
+    let mut tot = Duration::from_secs(0);
+
+    for _ in 0..samples {
+        let time = Instant::now();
+        for _ in 0..num {
+            func();
+        }
+        let elapsed = time.elapsed();
+        if elapsed > max {
+            max = elapsed;
+        }
+        if elapsed < min {
+            min = elapsed;
+        }
+        tot += elapsed;
+    }
+    
+    println!("Total elapsed: {:?}", tot);
+    let avg = (tot / 100) / num;
+    println!(
+        "[max: {:?}, min: {:?}, avg: {:?}]",
+        max / num,
+        min / num,
+        avg
+    );
+    return avg;
+}
+
+/// Wrapper function for `benchmark`. Tests the speed of the lexer
+pub fn lex_speed() {
+    let src = fs::read_to_string("scripts/example.risp")
+        .expect("Could not open scripts/example.risp");
+
+    // Function to benchmark. Creates a lexer and lexes the entire source file each time
+    // Source file must not contain any errors
+    let bench_fn = || {
+        let mut lexer = Lexer::new(&src);
+        while lexer.next().unwrap().kind == TokenKind::EOF { /* Benchmark */ }
+    };
+
+    let avg = bench(bench_fn, 50000, 1000);
+
+    // Calculates stats based on bench average
+    let bytes_per_sec = (1_000_000_000.0 / avg.as_nanos() as f64) * src.len() as f64;
+    let mb = 1000.0 * 1000.0;
+    let mib = 1024.0 * 1024.0;
+
+    println!(
+        "Average lex speed: {:.3} MB/s => {:.3} MiB/s",
+        bytes_per_sec / mb,
+        bytes_per_sec / mib
+    );
+}
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
-use std::time::{Duration, Instant};
 use std::{env, fs};
 use std::{io, io::prelude::*};
 
 mod risp;
 use risp::{AstNode, RispType, Type};
 
-use crate::risp::TokenKind;
+mod lexspeed;
 
 #[macro_use]
 extern crate lazy_static;
 
 /// Interprets multiple expressions using the same interpreter
-fn interpret_exprs(interpreter: &mut risp::Interpreter, asts: Vec<AstNode>) {
+fn interpret_exprs(interpreter: &mut risp::Interpreter, asts: Vec<AstNode>, output: bool) {
     for ast in asts.iter().cloned() {
         let value = interpreter.eval(ast);
 
         match value {
             Ok(Type::Null) => (),
-            Ok(v) => println!("\x1b[32m{}\x1b[0m", v.repr()),
-            Err(err) => eprintln!("\x1b[33m{err:?}\x1b[0m"),
+            Ok(v) if output => println!("{}", v.repr()),
+            Err(err) => eprintln!("{err:?}"),
+            _ => ()
         }
     }
 }
@@ -26,8 +26,7 @@ fn interpret_exprs(interpreter: &mut risp::Interpreter, asts: Vec<AstNode>) {
 fn repl() {
     let mut interpreter = risp::Interpreter::new();
 
-    // Initial greeting
-    print!(concat!("risp v0.5.0\n", "Type 'quit' to quit\n"));
+    println!("risp v0.6.0. Type 'quit' to quit");
 
     // REPL loop
     loop {
@@ -39,15 +38,13 @@ fn repl() {
             .read_line(&mut line)
             .expect("Failed to read line");
 
-        match &line.trim()[..] {
+        match line.trim() {
             "quit" => break,
 
             // Interprets all expressions on the line
             _ => match risp::to_ast(&mut line) {
-                Ok(ast) => {
-                    interpret_exprs(&mut interpreter, ast);
-                }
-                Err(err) => eprintln!("\x1b[33m{err:?}\x1b[0m"),
+                Ok(ast) => interpret_exprs(&mut interpreter, ast, true),
+                Err(err) => eprintln!("{err:?}"),
             },
         }
     }
@@ -60,78 +57,15 @@ fn run_file(filename: &str) {
     let mut interpreter = risp::Interpreter::new();
 
     match risp::to_ast(&mut src) {
-        Ok(asts) => {
-            for ast in asts.iter().cloned() {
-                match interpreter.eval(ast) {
-                    Ok(_) => (()),
-                    Err(err) => {
-                        eprintln!("\x1b[33m{err:?}\x1b[0m");
-                        break;
-                    }
-                }
-            }
-        }
-        Err(err) => eprintln!("\x1b[33m{err:?}\x1b[0m"),
+        Ok(asts) => interpret_exprs(&mut interpreter, asts, false),
+        Err(err) => eprintln!("{err:?}"),
     }
-}
-
-/// Used to benchmark a function
-fn bench(mut func: impl FnMut(), num: u32, samples: u32) -> Duration {
-    let mut max = Duration::from_secs(0);
-    let mut min = Duration::from_secs(u64::MAX);
-    let mut tot = Duration::from_secs(0);
-    for _ in 0..samples {
-        let time = Instant::now();
-        for _ in 0..num {
-            func();
-        }
-        let elapsed = time.elapsed();
-        if elapsed > max {
-            max = elapsed;
-        }
-        if elapsed < min {
-            min = elapsed;
-        }
-        tot += elapsed;
-    }
-    println!("Total elapsed: {:?}", tot);
-    let avg = (tot / 100) / num;
-    println!(
-        "[max: {:?}, min: {:?}, avg: {:?}]",
-        max / num,
-        min / num,
-        avg
-    );
-    return avg;
-}
-
-/// Wrapper function for `benchmark`. Tests the speed of the lexer
-fn lex_speed() {
-    let src =
-        fs::read_to_string("scripts/example.risp").expect("Could not open scripts/example.risp");
-
-    let bench_fn = || {
-        let mut lexer = risp::Lexer::new(&src);
-        while lexer.next().unwrap().kind == TokenKind::EOF { /* Benchmark */ }
-    };
-
-    let avg = bench(bench_fn, 5000, 100);
-
-    let bytes_per_sec = (1_000_000_000.0 / avg.as_nanos() as f64) * src.len() as f64;
-    let mb = 1000.0 * 1000.0;
-    let mib = 1024.0 * 1024.0;
-
-    println!(
-        "Average lex speed: {:.3} MB/s => {:.3} MiB/s",
-        bytes_per_sec / mb,
-        bytes_per_sec / mib
-    );
 }
 
 fn main() {
     match env::args().nth(1).as_deref() {
         None => repl(),
-        Some("bench") => lex_speed(),
+        Some("bench") => lexspeed::lex_speed(),
         Some(filename) => run_file(&filename),
     }
 }
