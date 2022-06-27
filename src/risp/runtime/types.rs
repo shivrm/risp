4f7756f69a548b10deb99cf70c001c6f3d393d9d
shivrm <shivrm@proton.me>
2022-06-27 22:08:25 +0530

    Split type system into multiple files

--- src/risp/runtime/types.rs
+++ src/risp/runtime/types.rs
@@ -1,13 +1,9 @@
-use crate::risp::{ Interpreter, AstNode, Op, RuntimeError };
+use crate::risp::{Op};
 
-pub type Int = i32;
-pub type Bool = bool;
-pub type Float = f64;
-pub type Str = String;
-pub type List = Vec<Type>;
-pub type RustFn = fn(List) -> Result<List, RuntimeError>;
-pub type RustMacro = fn(&mut Interpreter, Vec<AstNode>) -> Result<Type, RuntimeError>;
-pub struct Null;
+mod misc;
+mod num;
+
+use misc::*; use num::*;
 
 macro_rules! delegate {
     ($obj:ident, $name:ident, $( $x:expr ),*) => {
@@ -25,17 +21,16 @@ macro_rules! delegate {
     };
 }
 
-#[derive(Clone)]
-pub enum Type {
-    Int(Int),
-    Bool(bool),
-    Float(Float),
-    Str(Str),
-    List(List),
-    RustFn(RustFn),
-    RustMacro(RustMacro),
-    Operator(Op),
-    Null,
+macro_rules! derive_from {
+    ($($kind:ident),*) => {
+        $(
+        impl From<$kind> for Type {
+            fn from(item: $kind) -> Self {
+                Type::$kind(item)
+            }
+        }
+        )*
+    };
 }
 
 pub trait RispType {
@@ -68,6 +63,21 @@ pub trait RispType {
         None
     }
 }
+
+#[derive(Clone)]
+pub enum Type {
+    Int(Int),
+    Bool(Bool),
+    Float(Float),
+    Str(Str),
+    List(List),
+    RustFn(RustFn),
+    RustMacro(RustMacro),
+    Operator(Op),
+    Null,
+}
+
+derive_from!(Int, Bool, Float, Str, List, RustFn, RustMacro);
 
 // Function definitions could be done by a macro 🤔
 impl RispType for Type {
@@ -113,379 +123,5 @@ impl RispType for Type {
 
     fn rdiv(&self, other: &Type) -> Option<Type> {
         delegate!(self, div, other)
-    }
-}
-
-impl RispType for Int {
-    fn display(&self) -> String {
-        self.to_string()
-    }
-
-    fn repr(&self) -> String {
-        self.to_string()
-    }
-
-    fn type_name(&self) -> String {
-        return "int".into()
-    }
-
-    fn add(&self, other: &Type) -> Option<Type> {
-        match other {
-            Type::Int(n) => Some(Type::Int(self + n)),
-            _ => None,
-        }
-    }
-
-    fn sub(&self, other: &Type) -> Option<Type> {
-        match other {
-            Type::Int(n) => Some(Type::Int(self - n)),
-            _ => None,
-        }
-    }
-
-    fn mul(&self, other: &Type) -> Option<Type> {
-        match other {
-            Type::Int(n) => Some(Type::Int(self * n)),
-            _ => None,
-        }
-    }
-
-    fn div(&self, other: &Type) -> Option<Type> {
-        match other {
-            Type::Int(n) => Some(Type::Int(self / n)),
-            _ => None,
-        }
-    }
-}
-
-impl RispType for Bool {
-    fn display(&self) -> String {
-        self.to_string()
-    }
-
-    fn repr(&self) -> String {
-        self.to_string()
-    }
-
-    fn type_name(&self) -> String {
-        return "bool".into()
-    }
-
-    fn add(&self, other: &Type) -> Option<Type> {
-        match other {
-            Type::Bool(b) => Some(Type::Int(*self as i32 + *b as i32)),
-            Type::Int(n) => Some(Type::Int(*self as i32 + n)),
-            Type::Float(f) => Some(Type::Float(*self as i32 as f64 + f)),
-            _ => None,
-        }
-    }
-
-    fn sub(&self, other: &Type) -> Option<Type> {
-        match other {
-            Type::Bool(b) => Some(Type::Int(*self as i32 - *b as i32)),
-            Type::Int(n) => Some(Type::Int(*self as i32 - n)),
-            Type::Float(f) => Some(Type::Float(*self as i32 as f64 - f)),
-            _ => None,
-        }
-    }
-
-    fn mul(&self, other: &Type) -> Option<Type> {
-        match other {
-            Type::Bool(b) => Some(Type::Int(*self as i32 * *b as i32)),
-            Type::Int(n) => Some(Type::Int(*self as i32 * n)),
-            Type::Float(f) => Some(Type::Float(*self as i32 as f64 * f)),
-            _ => None,
-        }
-    }
-
-    fn div(&self, other: &Type) -> Option<Type> {
-        match other {
-            Type::Bool(b) => Some(Type::Int(*self as i32 / *b as i32)),
-            Type::Int(n) => Some(Type::Int(*self as i32 / n)),
-            Type::Float(f) => Some(Type::Float(*self as i32 as f64 / f)),
-            _ => None,
-        }
-    }
-
-    fn radd(&self, other: &Type) -> Option<Type> {
-        match other {
-            Type::Int(n) => Some(Type::Int(n + *self as i32)),
-            Type::Float(f) => Some(Type::Float(f + *self as i32 as f64)),
-            _ => None,
-        }
-    }
-
-    fn rsub(&self, other: &Type) -> Option<Type> {
-        match other {
-            Type::Int(n) => Some(Type::Int(n - *self as i32)),
-            Type::Float(f) => Some(Type::Float(f - *self as i32 as f64)),
-            _ => None,
-        }
-    }
-
-    fn rmul(&self, other: &Type) -> Option<Type> {
-        match other {
-            Type::Int(n) => Some(Type::Int(n * *self as i32)),
-            Type::Float(f) => Some(Type::Float(f * *self as i32 as f64)),
-            _ => None,
-        }
-    }
-
-    fn rdiv(&self, other: &Type) -> Option<Type> {
-        match other {
-            Type::Int(n) => Some(Type::Int(n / *self as i32)),
-            Type::Float(f) => Some(Type::Float(f / *self as i32 as f64)),
-            _ => None,
-        }
-    }
-}
-
-impl RispType for Float {
-    fn display(&self) -> String {
-        format!("{self:?}")
-    }
-
-    fn repr(&self) -> String {
-        format!("{self:?}")
-    }
-
-    fn type_name(&self) -> String {
-        return "float".into()
-    }
-
-    fn add(&self, other: &Type) -> Option<Type> {
-        match other {
-            Type::Int(n) => Some(Type::Float(self + f64::from(*n))),
-            Type::Float(f) => Some(Type::Float(self + f)),
-            _ => None,
-        }
-    }
-
-    fn sub(&self, other: &Type) -> Option<Type> {
-        match other {
-            Type::Int(n) => Some(Type::Float(self - f64::from(*n))),
-            Type::Float(f) => Some(Type::Float(self - f)),
-            _ => None,
-        }
-    }
-
-    fn mul(&self, other: &Type) -> Option<Type> {
-        match other {
-            Type::Int(n) => Some(Type::Float(self * f64::from(*n))),
-            Type::Float(f) => Some(Type::Float(self * f)),
-            _ => None,
-        }
-    }
-
-    fn div(&self, other: &Type) -> Option<Type> {
-        match other {
-            Type::Int(n) => Some(Type::Float(self / f64::from(*n))),
-            Type::Float(f) => Some(Type::Float(self / f)),
-            _ => None,
-        }
-    }
-
-    fn radd(&self, other: &Type) -> Option<Type> {
-        match other {
-            Type::Int(n) => Some(Type::Float(self + f64::from(*n))),
-            _ => None,
-        }
-    }
-
-    fn rsub(&self, other: &Type) -> Option<Type> {
-        match other {
-            Type::Int(n) => Some(Type::Float(f64::from(*n) - self)),
-            _ => None,
-        }
-    }
-
-    fn rmul(&self, other: &Type) -> Option<Type> {
-        match other {
-            Type::Int(n) => Some(Type::Float(self * f64::from(*n))),
-            _ => None,
-        }
-    }
-
-    fn rdiv(&self, other: &Type) -> Option<Type> {
-        match other {
-            Type::Int(n) => Some(Type::Float(f64::from(*n) / self)),
-            _ => None,
-        }
-    }
-}
-
-impl RispType for Str {
-    fn display(&self) -> String {
-        self.clone()
-    }
-
-    fn repr(&self) -> String {
-        format!("{self:?}")
-    }
-
-    fn type_name(&self) -> String {
-        return "str".into()
-    }
-
-    fn add(&self, other: &Type) -> Option<Type> {
-        match other {
-            Type::Str(s) => Some(Type::Str(self.clone() + &s)),
-            _ => None,
-        }
-    }
-
-    fn mul(&self, other: &Type) -> Option<Type> {
-        match other {
-            Type::Int(n) => Some(Type::Str(self.repeat(*n as usize))),
-            _ => None,
-        }
-    }
-
-    fn rmul(&self, other: &Type) -> Option<Type> {
-        match other {
-            Type::Int(n) => Some(Type::Str(self.repeat(*n as usize))),
-            _ => None,
-        }
-    }
-}
-
-impl RispType for List {
-    fn repr(&self) -> String {
-        let mut iter = self.iter();
-
-        let mut result = match iter.next() {
-            Some(el) => {
-                let r = el.repr();
-                format!(", {r}")
-            }
-            None => String::from("["),
-        };
-
-        for el in iter {
-            result += &format!(", {}", el.repr())
-        }
-
-        result.push(']');
-
-        result
-    }
-
-    fn display(&self) -> String {
-        let mut iter = self.iter();
-
-        let mut result = match iter.next() {
-            Some(el) => format!("[{}", el.display()),
-            None => String::from("["),
-        };
-
-        for el in iter {
-            result += &format!(", {}", el.display())
-        }
-
-        result.push(']');
-
-        result
-    }
-
-    fn type_name(&self) -> String {
-        return "list".into()
-    }
-
-    fn add(&self, other: &Type) -> Option<Type> {
-        match other {
-            Type::List(el) => Some(Type::List(
-                self.iter().cloned().chain(el.iter().cloned()).collect(),
-            )),
-            _ => None,
-        }
-    }
-
-    fn mul(&self, other: &Type) -> Option<Type> {
-        match other {
-            Type::Int(n) => Some(Type::List(
-                self.iter()
-                    .cloned()
-                    .cycle()
-                    .take(self.len() * *n as usize)
-                    .collect(),
-            )),
-            _ => None,
-        }
-    }
-
-    fn rmul(&self, other: &Type) -> Option<Type> {
-        match other {
-            Type::Int(n) => Some(Type::List(
-                self.iter()
-                    .cloned()
-                    .cycle()
-                    .take(self.len() * *n as usize)
-                    .collect(),
-            )),
-            _ => None,
-        }
-    }
-}
-
-impl RispType for RustFn {
-    fn display(&self) -> String {
-        "<Rust Function>".to_owned()
-    }
-
-    fn repr(&self) -> String {
-        self.display()
-    }
-
-    fn type_name(&self) -> String {
-        return "rustfn".into()
-    }
-}
-
-
-impl RispType for RustMacro {
-    fn display(&self) -> String {
-        "<Rust Macro>".to_owned()
-    }
-
-    fn repr(&self) -> String {
-        self.display()
-    }
-
-    fn type_name(&self) -> String {
-        return "rustmacro".into()
-    }
-}
-
-impl RispType for Op {
-    fn display(&self) -> String {
-        let value = match self {
-            Op::Plus => "+",
-            Op::Minus => "-",
-            Op::Star => "*",
-            Op::Slash => "/",
-        };
-        value.to_owned()
-    }
-
-    fn repr(&self) -> String {
-        self.display()
-    }
-
-    fn type_name(&self) -> String {
-        return "operator".into()
-    }
-}
-
-impl RispType for Null {
-    fn display(&self) -> String {
-        "Null".to_owned()
-    }
-
-    fn repr(&self) -> String {
-        String::new()
-    }
-
-    fn type_name(&self) -> String {
-        return "null".into()
     }
 }
